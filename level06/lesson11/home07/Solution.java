59ace67bca0a5b12ddc1bff819c4f558b81a2f59
andrey.gabchak@gmail.com <andrey.gabchak@gmail.com>
2016-03-26 20:03:56 +0200

    Setting up synchronization

--- level06/lesson11/home07/Solution.java
+++ level06/lesson11/home07/Solution.java
@@ -1,10 +1,7 @@
 package com.javarush.test.level06.lesson11.home07;
 
-<<<<<<< HEAD
-=======
 import java.util.ArrayList;
 
->>>>>>> 2fb5e8213875d54d9aa85a522823c454221381d4
 /* Три статические переменных name
 Создай 3 public статических переменных: String Solution.name, String Cat.name, String Dog.name
 */
