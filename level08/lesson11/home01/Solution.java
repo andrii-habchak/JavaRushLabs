b727b1d1f43af284683119ed0ca54466fbe0f699
andrey.gabchak@gmail.com <andrey.gabchak@gmail.com>
2016-04-10 12:33:15 +0300

    Deleted elements from Set

---
+++ level08/lesson11/home01/Solution.java
@@ -0,0 +1,56 @@
+package com.javarush.test.level08.lesson11.home01;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+/* Set из котов
+1. Внутри класса Solution создать public static класс кот – Cat.
+2. Реализовать метод createCats, он должен создавать множество (Set) котов и добавлять в него 3 кота.
+3. В методе main удалите одного кота из Set cats.
+4. Реализовать метод printCats, он должен вывести на экран всех котов, которые остались во множестве. Каждый кот с новой строки.
+*/
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        Set<Cat> cats = createCats();
+
+        Iterator<Cat> iterator = cats.iterator();
+        iterator.next();
+        iterator.remove();
+
+        printCats(cats);
+    }
+
+    public static Set<Cat> createCats()
+    {
+        Set<Cat> cats = new HashSet<>();
+
+        cats.add(new Cat("Mursik", 4, 6));
+        cats.add(new Cat("Barsik", 3, 5));
+        cats.add(new Cat("Zasranec", 1, 4));
+
+        return cats;
+    }
+
+    public static void printCats(Set<Cat> cats)
+    {
+        for (Cat cat : cats) {
+            System.out.println(cat);
+        }
+    }
+
+    private static class Cat {
+        private String name;
+        private int age;
+        private int weight;
+
+        public Cat(String name, int age, int weight) {
+            this.name = name;
+            this.age = age;
+            this.weight = weight;
+        }
+    }
+}
