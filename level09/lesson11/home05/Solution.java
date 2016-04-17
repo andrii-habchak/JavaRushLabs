05e4fe5b9a0862266c1ccd70fbf4b1678938bf9b
andrey.gabchak@gmail.com <andrey.gabchak@gmail.com>
2016-04-17 17:41:11 +0300

    sorting of vowels and consonants from the input string

---
+++ level09/lesson11/home05/Solution.java
@@ -0,0 +1,68 @@
+package com.javarush.test.level09.lesson11.home05;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/* Гласные и согласные буквы
+Написать программу, которая вводит с клавиатуры строку текста.
+Программа должна вывести на экран две строки:
+1. первая строка содержит только гласные буквы
+2. вторая - только согласные буквы и знаки препинания из введённой строки.
+Буквы соединять пробелом, каждая строка должна заканчиваться пробелом.
+
+Пример ввода:
+Мама мыла раму.
+Пример вывода:
+а а ы а а у
+М м м л р м .
+*/
+
+public class Solution
+{
+    public static void main(String[] args) throws Exception
+    {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String inputString = reader.readLine();
+
+        char[] inputChar = inputString.toCharArray();
+
+        ArrayList<Character> vowelList = new ArrayList<>();
+        ArrayList<Character> consonantsList = new ArrayList<>();
+
+        for (int i = 0; i < inputChar.length; i++) {
+            if (isVowel(inputChar[i])){
+                vowelList.add(inputChar[i]);
+            } else {
+                consonantsList.add(inputChar[i]);
+            }
+        }
+
+        for (Character character : vowelList) {
+            System.out.print(character + " ");
+        }
+        System.out.println();
+        for (Character character : consonantsList) {
+            System.out.print(character + " ");
+        }
+
+
+    }
+
+
+    public static char[] vowels = new char[]{'а', 'я', 'у', 'ю', 'и', 'ы', 'э', 'е', 'о', 'ё'};
+
+    //метод проверяет, гласная ли буква
+    public static boolean isVowel(char c)
+    {
+        c = Character.toLowerCase(c);  //приводим символ в нижний регистр - от заглавных к строчным буквам
+
+        for (char d : vowels)   //ищем среди массива гласных
+        {
+            if (c == d)
+                return true;
+        }
+        return false;
+    }
+}
