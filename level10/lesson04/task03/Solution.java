e85273e77ecd49f6c3f007028e3632042c480ae6
andrey.gabchak@gmail.com <andrey.gabchak@gmail.com>
2016-04-21 14:04:28 +0300

    conversion of types numbers

--- level10/lesson04/task03/Solution.java
+++ level10/lesson04/task03/Solution.java
@@ -13,7 +13,7 @@ public class Solution
     {
         float f = (float)128.50;
         int i = (int)f;
-        int b = (int)(i + f);
+        int b = (byte)(i + f);
         System.out.println(b);
     }
 }
