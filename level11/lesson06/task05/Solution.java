afad75ec660e64dab3177d5b093a2da210ddb081
andrey.gabchak@gmail.com <andrey.gabchak@gmail.com>
2016-04-22 00:08:59 +0300

    inheritance

---
+++ level11/lesson06/task05/Solution.java
@@ -0,0 +1,60 @@
+package com.javarush.test.level11.lesson06.task05;
+
+/* ИТ-компания
+Написать девять классов: Worker(сотрудник), Clerk (клерк), IT (ИТ-специалист), Programmer(программист), ProjectManager(менеджер проекта), CTO(технический директор), HR(рекрутер), OfficeManager(офис-менеджер), Cleaner (уборщик).
+Унаследовать программиста, менеджера проекта и технического директора от ИТ-специалиста.
+Унаследовать рекрутера, уборщика и офис-менеджера от клерка.
+Унаследовать клерка и ИТ-специалиста от сотрудника.
+*/
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+    }
+
+    public class Worker
+    {
+
+    }
+
+    public class Clerk
+    {
+
+    }
+
+    public class IT
+    {
+
+    }
+
+    public class Programmer
+    {
+
+    }
+
+    public class ProjectManager
+    {
+
+    }
+
+    public class CTO
+    {
+
+    }
+
+    public class OfficeManager
+    {
+
+    }
+
+    public class HR
+    {
+
+    }
+
+    public class Cleaner
+    {
+
+    }
+}
