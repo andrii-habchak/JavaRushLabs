8b1ae569617cebb6285aff833183af8ff2716770
andrey.gabchak@gmail.com <andrey.gabchak@gmail.com>
2016-04-22 00:30:06 +0300

    encapsulation

--- level11/lesson08/task05/Solution.java
+++ level11/lesson08/task05/Solution.java
@@ -1,40 +1,36 @@
-≈package com.javarush.test.level11.lesson08.task05;
+package com.javarush.test.level11.lesson08.task05;
 
 /* Добавь недостающие переменные
 Посмотри внимательно на методы и добавь недостающие переменные.
 */
 
-public class Solution
-{
-    public static void main(String[] args)
-    {
+public class Solution {
+    public static void main(String[] args) {
     }
 
-    public class Cat
-    {
+    public class Cat {
+        private String name;
+        private int age;
+        private int weight;
+        private int speed;
 
-        public Cat(String name, int age, int weight)
-        {
+        public Cat(String name, int age, int weight) {
 
         }
 
-        public String getName()
-        {
+        public String getName() {
             return null;
         }
 
-        public int getAge()
-        {
+        public int getAge() {
             return 0;
         }
 
-        public void setWeight(int weight)
-        {
+        public void setWeight(int weight) {
 
         }
 
-        public void setSpeed(int speed)
-        {
+        public void setSpeed(int speed) {
 
         }
     }
