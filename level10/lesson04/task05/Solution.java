3bf1cf9f30db6a4e2c6aa92895849e2889f768df
andrey.gabchak@gmail.com <andrey.gabchak@gmail.com>
2016-04-21 14:13:54 +0300

    conversion of types numbers

--- level10/lesson04/task05/Solution.java
+++ level10/lesson04/task05/Solution.java
@@ -12,8 +12,8 @@ public class Solution
     public static void main(String[] args)
     {
         int a = (byte)44;
-        int b = (byte)300;
-        short c = (byte)(b - a);
+        int b = 300;
+        short c = (short) (b - a);
         System.out.println(c);
     }
 }
