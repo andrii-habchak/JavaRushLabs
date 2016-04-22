0150f6a2b5b5e8b5f4cf19a1de2a7fa7d366669c
andrey.gabchak@gmail.com <andrey.gabchak@gmail.com>
2016-04-23 00:02:43 +0300

    method that returns the minimum number in the array and the index

---
+++ level12/lesson12/bonus03/Solution.java
@@ -0,0 +1,48 @@
+package com.javarush.test.level12.lesson12.bonus03;
+
+/* Задача по алгоритмам
+Написать метод, который возвращает минимальное число в массиве и его позицию (индекс).
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        int[] data = new int[]{1, 2, 3, 5, -2, -8, 0, 77, 5, 5};
+
+        Pair<Integer, Integer> result = getMinimumAndIndex(data);
+
+        System.out.println("Minimum is " + result.x);
+        System.out.println("Index of minimum element is " + result.y);
+    }
+
+    public static Pair<Integer, Integer> getMinimumAndIndex(int[] array) {
+        if (array == null || array.length == 0) {
+            return new Pair<Integer, Integer>(null, null);
+        }
+
+        //Напишите тут ваше решение
+        int length = array.length - 1;
+        int min = array[0];
+        int position = 0;
+
+        for (int i = length; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (min > array [j]) {
+                    min = array[j];
+                    position = j;
+                }
+            }
+        }
+        return new Pair<Integer, Integer>(min, position);
+    }
+
+
+    public static class Pair<X, Y> {
+        public X x;
+        public Y y;
+
+        public Pair(X x, Y y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
