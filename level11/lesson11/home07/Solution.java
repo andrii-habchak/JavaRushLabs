20e289e5f5eda9c6dcba0e3fd3ef3ff1a5dc9669
andrey.gabchak@gmail.com <andrey.gabchak@gmail.com>
2016-04-22 16:07:07 +0300

    inheritance

--- level11/lesson11/home07/Solution.java
+++ level11/lesson11/home07/Solution.java
@@ -1,37 +1,31 @@
 package com.javarush.test.level11.lesson11.home07;
 
 /* Вторая правильная «цепочка наследования»
-Расставь правильно «цепочку наследования» в классах: Carnivora (плотоядное животное), Cow (корова), Dog(собака), Pig(свинья), Animal (животное).
+Расставь правильно «цепочку наследования» в классах:
+Carnivora (плотоядное животное), Cow (корова), Dog(собака), Pig(свинья), Animal (животное).
 */
 
-public class Solution
-{
-    public static void main(String[] args)
-    {
+public class Solution {
+    public static void main(String[] args) {
     }
 
-    public class Carnivora
-    {
+    public class Carnivora extends Animal {
 
     }
 
-    public class Cow
-    {
+    public class Cow extends Animal {
 
     }
 
-    public class Dog
-    {
+    public class Dog extends Carnivora {
 
     }
 
-    public class Pig
-    {
+    public class Pig extends Animal {
 
     }
 
-    public class Animal
-    {
+    public class Animal {
 
     }
 }
