d81866f5d90c9862fabb86d2307a015294dd3951
andrey.gabchak@gmail.com <andrey.gabchak@gmail.com>
2016-04-22 21:18:49 +0300

    polymorphism. method overloading

---
+++ level12/lesson04/task03/Solution.java
@@ -0,0 +1,33 @@
+package com.javarush.test.level12.lesson04.task03;
+
+/* Пять методов print с разными параметрами
+Написать пять методов print с разными параметрами.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    //Напишите тут ваши методы
+    public int print(int n){
+        return n;
+    }
+
+    public String print(String n){
+        return n;
+    }
+
+    public char print(char n){
+        return n;
+    }
+
+    public byte print(byte n){
+        return n;
+    }
+
+    public double print(double n){
+        return n;
+    }
+
+}
