c65280fa086a1006e2852b99e6bf7f7188cf6ff7
andrey.gabchak@gmail.com <andrey.gabchak@gmail.com>
2016-04-23 19:25:00 +0300

    interface

--- level13/lesson02/task03/Solution.java
+++ level13/lesson02/task03/Solution.java
@@ -4,31 +4,33 @@ package com.javarush.test.level13.lesson02.task03;
 Реализуй интерфейс Drink в классах Beer и Cola.
 */
 
-public class Solution
-{
-    public static void main(String[] args) throws Exception
-    {
+public class Solution {
+    public static void main(String[] args) throws Exception {
 
         print(new Beer());
         print(new Cola());
 
     }
 
-    private static void print(Drink drink)
-    {
+    private static void print(Drink drink) {
         System.out.println(drink.getClass().getSimpleName());
     }
 
-    public interface Drink
-    {
+    public interface Drink {
         boolean isAlcoholic();
     }
 
-    public static class Beer
-    {
+    public static class Beer implements Drink {
+        @Override
+        public boolean isAlcoholic() {
+            return false;
+        }
     }
 
-    public static class Cola
-    {
+    public static class Cola implements Drink {
+        @Override
+        public boolean isAlcoholic() {
+            return false;
+        }
     }
 }
