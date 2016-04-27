516e4328f3607e64bedc7002d087530e79222ff3
andrey.gabchak@gmail.com <andrey.gabchak@gmail.com>
2016-04-27 23:18:25 +0300

    Inheritance of class

---
+++ level14/lesson08/home05/CompItem.java
@@ -0,0 +1,8 @@
+package com.javarush.test.level14.lesson08.home05;
+
+/**
+ * Created by coura on 27.04.2016.
+ */
+interface CompItem {
+    String getName();
+}
---
+++ level14/lesson08/home05/Mouse.java
@@ -0,0 +1,11 @@
+package com.javarush.test.level14.lesson08.home05;
+
+/**
+ * Created by coura on 27.04.2016.
+ */
+class Mouse implements CompItem {
+    @Override
+    public String getName() {
+        return "Mouse";
+    }
+}
