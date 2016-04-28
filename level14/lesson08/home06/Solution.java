09dd4ac533ca67103b04ab99e36675377a980d86
andrey.gabchak@gmail.com <andrey.gabchak@gmail.com>
2016-04-28 12:11:34 +0300

    Inheritance of class

---
+++ level14/lesson08/home06/Solution.java
@@ -0,0 +1,104 @@
+package com.javarush.test.level14.lesson08.home06;
+
+/* MovieFactory
+Расширение функционала по аналогии, чтение с консоли:
+1. Разобраться, что программа умеет делать.
+2. Все классы должны быть внутри класса Solution.
+3. Добавить классы Cartoon, Thriller.
+4. Разобраться, как мы получаем объект класса SoapOpera по ключу "soapOpera".
+Аналогично получению объекта SoapOpera сделать:
+5. Добавить в MovieFactory.getMovie получение объекта Cartoon для ключа "cartoon".
+6. Добавить в MovieFactory.getMovie получение объекта Thriller для ключа "thriller".
+
+7. Считать с консоли несколько ключей (строк).
+7.1. Ввод заканчивается, как только вводится строка не совпадающая с одной из: "cartoon", "thriller", "soapOpera".
+8. Создать переменную movie класса Movie и для каждой введенной строки(ключа):
+8.1. Получить объект используя MovieFactory.getMovie и присвоить его переменной movie.
+8.2. Вывести на экран movie.getClass().getSimpleName().
+*/
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //ввести с консоли несколько ключей (строк), пункт 7
+        ArrayList<String> keys = enterKeys();
+        ArrayList<Movie> movies = newMovies(keys);
+
+        printMovies(movies);
+
+        /*
+8 Создать переменную movie класса Movie и для каждой введенной строки(ключа):
+8.1 получить объект используя MovieFactory.getMovie и присвоить его переменной movie
+8.2 вывести на экран movie.getClass().getSimpleName()
+        */
+
+    }
+
+    public static void printMovies(ArrayList<Movie> movies) {
+        for (Movie movie : movies) {
+            System.out.println(movie.getClass().getSimpleName());
+        }
+    }
+
+    public static ArrayList<Movie> newMovies(ArrayList<String> keys) {
+        ArrayList<Movie> movies = new ArrayList<>();
+
+        for (String key : keys) {
+            movies.add(MovieFactory.getMovie(key));
+        }
+        return movies;
+    }
+
+
+    public static ArrayList<String> enterKeys() throws IOException {
+        ArrayList<String> keys = new ArrayList<>();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true) {
+            String key = reader.readLine();
+            if ((key.equals("soapOpera")) || (key.equals("cartoon")) || (key.equals("thriller"))) {
+                keys.add(key);
+            } else break;
+        }
+        return keys;
+    }
+
+    static class MovieFactory {
+
+        static Movie getMovie(String key) {
+            Movie movie = null;
+
+            //создание объекта SoapOpera (мыльная опера) для ключа "soapOpera"
+            if (key.equals("soapOpera")) {
+                movie = new SoapOpera();
+            } else if (key.equals("cartoon")) {
+                movie = new Cartoon();
+            } else if (key.equals("thriller")) {
+                movie = new Thriller();
+            }
+
+            //напишите тут ваш код, пункты 5,6
+
+            return movie;
+        }
+    }
+
+    static abstract class Movie {
+    }
+
+    static class SoapOpera extends Movie {
+    }
+
+    //Напишите тут ваши классы, пункт 3
+    static class Cartoon extends Movie{
+
+    }
+
+    static class Thriller extends Movie {
+
+    }
+}
